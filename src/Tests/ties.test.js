933b924e6a6dbd089202be2eb4d3143f705edd30
mikefranze <41272412+mikefranze@users.noreply.github.com>
2021-01-17 21:59:59 -0800

    initial commit

--- src/Tests/ties.test.js
+++ src/Tests/ties.test.js
@@ -60,3 +60,38 @@ test("True Tie Test", () => {
   const expected = [["Allison", "Bill", "Carmen"], [], ["Doug"]];
   expect(Winners(single)).toEqual(expected);
 });
+
+test("PR Ties", () => {
+  const csv = `Allison	Bill	Carmen	Doug
+  5	4	1	4
+  5	4	1	4
+  2	4	1	2
+  4	3	2	1
+  0	5	4	4
+  3	2	4	2
+  3	1	5	3
+  3	1	5	3
+  1	3	2	2
+  4	3	5	5
+  `;
+
+  const results = ElectionResultsFromText(csv);
+  //propWinners = [];
+  //results.prop.winners.foreach((w,i) => {
+  //  propWinners.push(w.name);
+  //});
+  const expectedWinners = ["Allison", "Carmen", "Bill"];
+
+  //const expected = ["c4", "c1", "c1"];
+  var propWinners = [];
+  var winners = results.pr.winners;
+  for (let i = 0; i < winners.length; i++) {
+    propWinners.push(winners[i].name);
+  }
+
+  //console.log(propWinners);
+  //console.log(results.pr);
+
+  expect(propWinners).toEqual(expectedWinners);
+  expect(3).toEqual(results.pr.ties[0].length);
+});
